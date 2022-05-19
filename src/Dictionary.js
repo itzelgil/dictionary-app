2ce237a1a8b0adc4f63bde044a9cf2bc5c425f95
Itzel Gil <79197169+itzelgil@users.noreply.github.com>
2022-05-19 14:35:17 +0200

    api doc link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,7 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-
+    //Documentation: https://dictionaryapi.dev
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     console.log(apiUrl);
     axios.get(apiUrl).then(handleResponse);
